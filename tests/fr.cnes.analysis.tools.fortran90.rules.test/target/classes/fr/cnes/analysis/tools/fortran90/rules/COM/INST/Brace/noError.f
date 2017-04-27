5d4c6b5699f0c2e06be7ada38396fa7c5397fb09
Olivier Prouvost <olivier.prouvost@opcoach.com>
2017-04-27 15:08:03 +0200

    Remove target directories
    Change-Id: I12a2694c075b08801241fec3e69dd6355093b037
    Signed-off-by: Olivier Prouvost <olivier.prouvost@opcoach.com>

--- tests/fr.cnes.analysis.tools.fortran90.rules.test/target/classes/fr/cnes/analysis/tools/fortran90/rules/COM/INST/Brace/noError.f
+++
@@ -1,5 +0,0 @@
-PROGRAM ESSAI
-	a = (b + c) - (((d * e) * f) ** (g + h))
-	a = .NOT. (a .EQ. (b .LT. (c .GE.d)))
-	a = (x(b) + y(b)) * x(b)
-END PROGRAM ESSAI
--- tests/fr.cnes.analysis.tools.fortran90.rules.test/target/classes/fr/cnes/analysis/tools/fortran90/rules/F90/INST/Nullify/noError.f
+++
@@ -1,9 +0,0 @@
-program ESSAI
-
-	allocate(C(n1), STAT = iom)
-
-	deallocate(C, stat=iom)
-	nullify  (C)
-	! pointeur NULL
-
-end program
