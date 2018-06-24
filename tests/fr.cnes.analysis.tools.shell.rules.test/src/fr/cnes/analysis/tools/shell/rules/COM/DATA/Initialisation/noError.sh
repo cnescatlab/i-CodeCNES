6ae21bc982fb87a769027613bcb1355ca6c9ad1f
Brigitte Huynh <brigitte.huynh@cnes.fr>
2018-06-24 19:54:14 +0200

    #113 Updated Unit Tests to take function localization into account

--- tests/fr.cnes.analysis.tools.shell.rules.test/src/fr/cnes/analysis/tools/shell/rules/COM/DATA/Initialisation/noError.sh
+++ tests/fr.cnes.analysis.tools.shell.rules.test/src/fr/cnes/analysis/tools/shell/rules/COM/DATA/Initialisation/noError.sh
@@ -7,3 +7,27 @@ echo "------------------------------------------"
 x=1
 y=2
 z=$x+$y
+
+function function1 ()
+{
+  b = 2
+  c = 3
+  a = $b+$c
+}
+
+# main
+xx=y
+
+function2 ()
+{
+  b = 2
+  a = $b+$xx
+  function function3 ()
+  {
+    e = $a
+  }
+  
+  
+}
+
+yy=$xx
