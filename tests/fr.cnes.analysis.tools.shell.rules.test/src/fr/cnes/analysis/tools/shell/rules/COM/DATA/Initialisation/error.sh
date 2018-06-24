65db14181f35f24bad41d5552c508d23c8a160e1
Brigitte HUYNH <brigitte.huynh@cnes.fr>
2018-06-24 19:54:14 +0200

    #113 Updated Unit Tests to take function localization into account

--- tests/fr.cnes.analysis.tools.shell.rules.test/src/fr/cnes/analysis/tools/shell/rules/COM/DATA/Initialisation/error.sh
+++ tests/fr.cnes.analysis.tools.shell.rules.test/src/fr/cnes/analysis/tools/shell/rules/COM/DATA/Initialisation/error.sh
@@ -5,22 +5,33 @@ echo "Fichier KO de TU"
 echo "------------------------------------------"
 
 x=1
-# la variable y n'est pas initiliaze
+# the y variable is not initialized
 z=$x+$y
 
-function fonction ()
+function function1 ()
 {
   b = 2
+  # the c variable is not initialized
   a = $b+$c
 }
 
-# main
-xx=y
+# main - the y variable is not initialized
 
-fonction2 ()
+xx=$y
+
+function2 ()
 {
-  b = 2
-  a = $b+$c
+  # the b variable is not initialized
+  a = $b
+  # the y variable is not initialized
+  d = $y
+  
+  function function3 ()
+  {
+    # the y variable is not initialized
+    e = $y
+  }
 }
 
-yy=y
+# the y variable is not initialized
+yy=$y
