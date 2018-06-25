12ca25f56596164777c25087131d64d018b2339a
Brigitte Huynh <brigitte.huynh@cnes.fr>
2018-06-25 15:26:29 +0200

    #122 Added violation location and string handling and updated unit tests accordingly

--- tests/fr.cnes.analysis.tools.shell.rules.test/src/fr/cnes/analysis/tools/shell/rules/SH/REF/Export/error.sh
+++ tests/fr.cnes.analysis.tools.shell.rules.test/src/fr/cnes/analysis/tools/shell/rules/SH/REF/Export/error.sh
@@ -21,3 +21,10 @@ export -f verifie_seuil_shell
 nom_script=$(basename $0)
 echo "Variable exportee au $nom_script s'appelle MAX_SHELL_LEVEL"
 echo "Founction exportee au $nom_script s'appelle verifie_seuil_shell"
+
+function testFunction ()
+{
+	export -f verifie_seuil_shell
+}
+
+export -f testFunction
