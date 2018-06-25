76352295293f2813888a8abeb75b79ce2d6d1ee6
Brigitte HUYNH <brigitte.huynh@cnes.fr>
2018-06-25 16:18:07 +0200

    #122 Updated a string and a comment to contain function to test that strings and comments are handled correctly

--- tests/fr.cnes.analysis.tools.shell.rules.test/src/fr/cnes/analysis/tools/shell/rules/SH/REF/Export/error.sh
+++ tests/fr.cnes.analysis.tools.shell.rules.test/src/fr/cnes/analysis/tools/shell/rules/SH/REF/Export/error.sh
@@ -14,13 +14,13 @@ verifie_seuil_shell()
 # --- Export d'une constante 
 typeset -r MAX_SHELL_LEVEL=2
 export MAX_SHELL_LEVEL
-# --- Export d'une certaine fonctions 
+# --- Export d'une certaine function
 export -f verifie_seuil_shell
 
 # --- Variables locales 
 nom_script=$(basename $0)
 echo "Variable exportee au $nom_script s'appelle MAX_SHELL_LEVEL"
-echo "Founction exportee au $nom_script s'appelle verifie_seuil_shell"
+echo "function exportee au $nom_script s'appelle verifie_seuil_shell"
 
 function testFunction ()
 {
