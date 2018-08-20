4be25f4ced26b7ed12074463f2886287f5ab905b
Brigitte Huynh <brigitte.huynh@cnes.fr>
2018-08-20 18:30:03 +0200

    #119 Corrected the issues of the COM.PRES.Indent rule:  - Awk command handling  - Quotes and double quotes handling  - Cases statements handling  - Analysis failure on braces (issue due to [[)
    The following issues no longer occur although there was no specific
    correction for them. The overall modifications of the rule must have
    resolved them:
     - Comments handling
     - Forbidden increasing of identation not handled

--- tests/fr.cnes.analysis.tools.shell.rules.test/src/fr/cnes/analysis/tools/shell/rules/COM/PRES/Indent/noError.sh
+++ tests/fr.cnes.analysis.tools.shell.rules.test/src/fr/cnes/analysis/tools/shell/rules/COM/PRES/Indent/noError.sh
@@ -5,6 +5,8 @@ echo "Fichier OK de TU"
 echo "La indentation du code source est bonne"
 echo "------------------------------------------"
 
+[[ $? != 0 ]] && { echo >&2 the required \"message\" command is not in your PATH; exit 1; }
+
 code_erreur_nbargs=-128
 
 # Définition de la variable qui contiendra le code retour (en particulier pour les cas d'erreur avec valeurs negatives)
@@ -55,7 +57,6 @@ ma_fonction_affine ()
 #    Definition d'une fonction qui affiche les resultats obtenus 
 # ------------------------------------------------------------------------------------
 affiche_resultat ()
-{
    if [ $# -ne 2 ]
    then
       printf "Erreur grave dans affiche_resultat : nombre d'arguments incorrects\n"
@@ -70,7 +71,6 @@ affiche_resultat ()
          printf "   ===>Erreur grave dans ma_fonction_affine : nombre d'arguments incorrects<===\n"
       fi
    fi
-}
 
 a_trouver=$(($RANDOM % 100))
  
@@ -86,4 +86,135 @@ do
         read i
 done
 echo "bravo, le nombre etait en effet $a_trouver"
+
+# - Traitement du fichier des mouvements, impression de chaque 
+# - mouvement avec son type et calcul du solde bancaire
+cat mvts.txt | awk 'BEGIN { print "Start AWK command" }
+                    /^A/ {print "A---", NR }
+                    /^B/ {print "B---", NR }
+                    /^C/ {print "C---", NR }
+                    /^D/ {print "D---", NR }
+                    /^E/ {print "E---", NR }
+                    /^F/ {print "F---", NR }
+                    /^![A-F]/ {print "Default---", NR }
+                    END { print "End AWK command" }'
+
+echo
+echo "String operations using \"expr \$string : \" construct"
+echo "==================================================="
+echo
+
+a=1234zipper5FLIPPER43231
+
+echo "The string being operated upon is \"`expr "$a" : '\(.*\)'`\"."
+#     Escaped parentheses grouping operator.            ==  ==
+
+#       ***************************
+#+          Escaped parentheses
+#+           match a substring
+#       ***************************
+
+
+#  If no escaped parentheses...
+#+ then 'expr' converts the string operand to an integer.
+
+echo "Length of \"$a\" is `expr "$a" : '.*'`."   # Length of string
+
+echo "Number of digits at the beginning of \"$a\" is `expr "$a" : '[0-9]*'`."
+
+# ------------------------------------------------------------------------- #
+
+echo
+
+echo "The digits at the beginning of \"$a\" are `expr "$a" : '\([0-9]*\)'`."
+#                                                             ==      ==
+echo "The first 7 characters of \"$a\" are `expr "$a" : '\(.......\)'`."
+#         =====                                          ==       ==
+# Again, escaped parentheses force a substring match.
+#
+echo "The last 7 characters of \"$a\" are `expr "$a" : '.*\(.......\)'`."
+#         ====                  end of string operator  ^^
+#  (actually means skip over one or more of any characters until specified
+#+  substring)
+
+echo
+
+
+INIT_TAB_AWK=""
+# Parameter to initialize awk script.
+count_case=0
+FILE_PARSE=$1
+
+E_PARAMERR=65
+
+usage()
+{
+    echo "Usage: letter-count.sh file letters" 2>&1
+    # For example:   ./letter-count2.sh filename.txt a b c
+    exit $E_PARAMERR  # Not enough arguments passed to script.
+}
+
+if [ ! -f "$1" ] ; then
+    echo "$1: No such file." 2>&1
+    usage                 # Print usage message and exit.
+fi 
+
+if [ -z "$2" ] ; then
+    echo "$2: No letters specified." 2>&1
+    usage
+fi 
+
+shift                      # Letters specified.
+for letter in `echo $@`    # For each one . . .
+do
+  INIT_TAB_AWK="$INIT_TAB_AWK tab_search[${count_case}] = \"$letter\"; final_tab[${count_case}] = 0; " 
+  # Pass as parameter to awk script below.
+  count_case=`expr $count_case + 1`
+done
+
+# DEBUG:
+# echo $INIT_TAB_AWK;
+
+cat $FILE_PARSE |
+# Pipe the target file to the following awk script.
+
+# ----------------------------------------------------------------------------------
+# Earlier version of script used:
+# awk -v tab_search=0 -v final_tab=0 -v tab=0 -v nb_letter=0 -v chara=0 -v chara2=0 \
+
+awk \
+"BEGIN { $INIT_TAB_AWK } \
+{ split(\$0, tab, \"\"); \
+for (chara in tab) \
+{ for (chara2 in tab_search) \
+{ if (tab_search[chara2] == tab[chara]) { final_tab[chara2]++ } } } } \
+END { for (chara in final_tab) \
+{ print tab_search[chara] \" => \" final_tab[chara] } }"
+# ----------------------------------------------------------------------------------
+#  Nothing all that complicated, just . . .
+#+ for-loops, if-tests, and a couple of specialized functions.
+
+
+# -----------------------------------------------------------------------
+# Every file has an inode, a record that holds its physical address info.
+# -----------------------------------------------------------------------
+
+echo; echo -n "Are you absolutely sure you want to delete \"$1\" (y/n)? "
+# The '-v' option to 'rm' also asks this.
+read answer
+case "$answer" in
+  [nN]) echo "Changed your mind, huh?"
+      exit $E_CHANGED_MIND
+      ;;
+  *)    echo "Deleting file \"$1\".";;
+esac
+
+awk '
+{nodecount()}  # Execute the user-defined nodecount() for each row to update the node count
+$0~/<clusternode /,/<\/clusternode>/{ print } # Print the rows between "<clusternode " and  "</clusternode>"
+function nodecount() #Function definition for nodecount
+{if($0~/clusternode name/){count+=1}
+}
+END{print "Node Count="count}' $1 #Print the count value in the END pattern which is executed once
+
 
