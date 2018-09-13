6b553e25e96a79049e613e8f07b5f16fe0ce83c2
Brigitte Huynh <brigitte.huynh@cnes.fr>
2018-09-13 14:17:35 +0200

    #150 Localization of the COM.FLOW.FileExistence rule. Test file updated accordingly

--- tests/fr.cnes.analysis.tools.shell.rules.test/src/fr/cnes/analysis/tools/shell/rules/COM/FLOW/FileExistence/error.sh
+++ tests/fr.cnes.analysis.tools.shell.rules.test/src/fr/cnes/analysis/tools/shell/rules/COM/FLOW/FileExistence/error.sh
@@ -19,6 +19,7 @@ grep ^2 $fictle > l1.tmp
 
 date>$fichier
 
+my-function ()
 if [ -e ${TAPE_DEV_REP}/${DATE_EXE}_${TYPE_ACTION}_${NIVEAU_SAUVEGARDE}_${MACHINE_S}.tar.gz ] ; then   
    cat "${DATE_EXE}_${TYPE_ACTION}_${NIVEAU_SAUVEGARDE}_${MACHINE_S}.tar.gz" | Execute_Commande_C_Retour -d "cat - > ${TAPE_DEV_REP}/${DATE_EXE}_${TYPE_ACTION}_${NIVEAU_SAUVEGARDE}_${MACHINE_S}.tar.gz"
 fi
