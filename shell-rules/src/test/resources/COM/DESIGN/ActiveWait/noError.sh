1f4edb5fc422c9ebe573082d969eed4ced84226e
Benoît Garçon <benoit.garcon@outlook.com>
2020-04-06 01:25:55 +0200

    Fix activewait as asked in #131

--- shell-rules/src/test/resources/COM/DESIGN/ActiveWait/noError.sh
+++ shell-rules/src/test/resources/COM/DESIGN/ActiveWait/noError.sh
@@ -11,3 +11,21 @@ do
    echo "Iteration num $i"
    i=$(($i+1))
 done
+
+while IFS= read -r line
+do
+   ## take some action on $line
+  echo "$line"
+done < <(ps -edf)
+
+while IFS= read line
+do
+   ## take some action on $line
+  echo "$line"
+done < <(ps -edf)
+
+while read line
+do
+   ## take some action on $line
+  echo "$line"
+done < <(ps -edf)
